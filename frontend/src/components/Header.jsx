92b66df16de281bc53b3b5ce1f0167a09412baf8
RFID-NC <stoyann.noumea@outlook.com>
2024-01-02 13:24:19 +1100

    add product card

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,22 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaInfoCircle, FaUser } from 'react-icons/fa';
-
+import logo from '../assets/logo.png';
 const Header = () => {
   return (
-    <header>
-      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+    <header >
+      <Navbar expand='md' collapseOnSelect>
+      {/* <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect> */}
         <Container>
-          <Navbar.Brand href='/'>Krysto</Navbar.Brand>
+          <Navbar.Brand href='/'>
+            <img className='logo' src={logo} alt="Logo Krysto" />
+           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
+              <Nav.Link href='/cart' className='header_link'>
                 <FaInfoCircle/> A propos
               </Nav.Link>
-              <Nav.Link href='/login'>
+              <Nav.Link href='/login'  className='header_link'>
                 <FaUser /> Connexion
               </Nav.Link>
             </Nav>
