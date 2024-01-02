7093e4f94e4b4b6c3dfb5e7c197dce8633767e61
RFID-NC <stoyann.noumea@outlook.com>
2024-01-02 14:03:56 +1100

    product card and rating components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,36 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaInfoCircle, FaUser } from 'react-icons/fa';
-import logo from '../assets/logo.png';
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { FaInfoCircle, FaUser } from 'react-icons/fa'
+import { LinkContainer } from 'react-router-bootstrap'
+import logo from '../assets/logo.png'
 const Header = () => {
   return (
-    <header >
-      <Navbar expand='md' collapseOnSelect>
-      {/* <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect> */}
+    <header>
+      <Navbar expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <img className='logo' src={logo} alt="Logo Krysto" />
-           </Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
-              <Nav.Link href='/cart' className='header_link'>
-                <FaInfoCircle/> A propos
-              </Nav.Link>
-              <Nav.Link href='/login'  className='header_link'>
-                <FaUser /> Connexion
-              </Nav.Link>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img className="logo" src={logo} alt="Logo Krysto" />
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/About">
+                <Nav.Link className="header_link">
+                  <FaInfoCircle /> A propos
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link className="header_link">
+                  <FaUser /> Connexion
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
---
+++ frontend/src/screens/NotFoundScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFoundScreen = () => {
+  return (
+    <>
+    <h1>OUPS</h1>
+<h2>La page que vous demandée n'existe pas</h2>
+
+<button>Retour</button>
+    </>
+  )
+}
+
+export default NotFoundScreen
